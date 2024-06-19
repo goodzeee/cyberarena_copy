815af8efab6eb7cbbbbdcce9fd42de74fe9a0efa
sedica3340 <sedica3340@gmail.com>
2024-06-19 16:48:04 +0900

    임포트 주소오류 수정

--- src/main/java/com/project/mvc/service/jihye/ReviewService.java
+++ src/main/java/com/project/mvc/service/jihye/ReviewService.java
@@ -1,11 +1,12 @@
 package com.project.mvc.service.jihye;
 
 
+import com.project.mvc.common.zyo.Search;
 import com.project.mvc.dto.response.jihye.ReviewFindAllDto;
 import com.project.mvc.dto.response.jihye.ReviewListDto;
 import com.project.mvc.entity.Media;
 import com.project.mvc.entity.Review;
-import com.project.mvc.mapper.MediaMapper;
+import com.project.mvc.mapper.zyo.MediaMapper;
 import com.project.mvc.mapper.jihye.ReviewMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +24,7 @@ public class ReviewService {
     private final ReviewMapper reviewMapper;
 
     public List<ReviewListDto> findAllMediaWithReviews() {
-        List<Media> mediaList = mediaMapper.findAll();
+        List<Media> mediaList = mediaMapper.findAll(Search.builder().build());
 
         return mediaList.stream()
                 .map(media -> {
